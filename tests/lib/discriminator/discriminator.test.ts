b878565c0d6c633bdb07d3030f424ae83dc361f4
blwatkins <blwatkins@users.noreply.github.com>
2026-03-20 15:18:43 -0500

    Add initial Discriminator unit tests.

--- tests/lib/discriminator/discriminator.test.ts
+++ tests/lib/discriminator/discriminator.test.ts
@@ -18,6 +18,55 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { test } from 'vitest';
+import { describe, test, expect } from 'vitest';
 
-test.todo('Discriminator unit tests');
+import {ASPECT_RATIOS, Discriminator} from "../../../src/lib";
+
+describe('Discriminator', (): void => {
+    const FAILURE_INPUTS: unknown[] = [
+        10,
+        10.10,
+        Number.MIN_SAFE_INTEGER,
+        Number.MAX_SAFE_INTEGER,
+        Number.MIN_VALUE,
+        Number.MIN_VALUE,
+        Math.random(),
+        NaN,
+        'Hello, World!',
+        '',
+        true,
+        false,
+        null,
+        undefined,
+        {},
+        { DISCRIMINATOR: '' },
+        { DISCRIMINATOR: 'SOME_VALUE' },
+        document,
+        Math.random,
+        (): number => {
+            return 5;
+        }
+    ];
+
+    const FAILURE_CASES: { input: unknown; expected: false }[] = FAILURE_INPUTS.map((input: unknown): { input: unknown; expected: false } => ({ input: input, expected: false }));
+
+    describe('new Discriminator()', (): void => {
+        test('Discriminator constructor should throw an Error', (): void => {
+            expect(() => new Discriminator()).toThrow('Discriminator is a static class and cannot be instantiated.');
+        });
+    });
+
+    // TODO - test all ASPECT_RATIOS - add to test cases by iterating through the Record
+    describe('Discriminator.isAspectRatioConfig()', (): void => {
+       const TEST_CASES:  { input: unknown; expected: boolean }[] = [
+           ...FAILURE_CASES,
+           { input: ASPECT_RATIOS.SQUARE, expected: true },
+       ];
+
+       test.each(
+           TEST_CASES
+       )('Discriminator.isAspectRatioConfig($input) should return $expected', ({ input, expected }): void => {
+              expect(Discriminator.isAspectRatioConfig(input)).toBe(expected);
+       });
+    });
+});
