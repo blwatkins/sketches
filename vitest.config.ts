630efe442cb543e3a58b811fb92bd440180fa3cc
blwatkins <blwatkins@users.noreply.github.com>
2026-03-23 10:43:45 -0500

    Refactor tests/ directory to test/

--- vitest.config.ts
+++ vitest.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        include: ['tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
         exclude: ['node_modules', '_dist', '_compiled', '_doc', 'docs'],
         coverage: {
             provider: 'v8',
