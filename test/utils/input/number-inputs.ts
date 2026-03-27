025062c07e6c73767064d7d913203bf7181c9756
blwatkins <blwatkins@users.noreply.github.com>
2026-03-27 15:56:24 -0500

    Add number-inputs.ts test utils.

---
+++ test/utils/input/number-inputs.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2026 Brittni Watkins.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// noinspection JSPrimitiveTypeWrapperUsage
+export const NON_NUMBER_INPUTS: unknown[] = [
+    null,
+    undefined,
+    '',
+    'string',
+    '\n\t',
+    '     ',
+    {},
+    { key: 'value' },
+    (): number => 10,
+    Math.random,
+    true,
+    false,
+    '5',
+    '5.5',
+    new Number(10),
+    10n,
+    [],
+    ['value']
+];
+
+export const NON_FINITE_NUMBER_INPUTS: number[] = [
+    NaN,
+    Infinity,
+    -Infinity
+];
+
+export const POSITIVE_NUMBER_INPUTS: number[] = [
+    10,
+    10.01,
+    Number.MIN_VALUE,
+    Number.MAX_VALUE,
+    Number.MAX_SAFE_INTEGER,
+    Number.EPSILON
+];
+
+export const NEGATIVE_NUMBER_INPUTS: number[] = [
+    -10,
+    -10.01,
+    -Number.MIN_VALUE,
+    -Number.MAX_VALUE,
+    -Number.MAX_SAFE_INTEGER,
+    -Number.EPSILON,
+    Number.MIN_SAFE_INTEGER
+];
+
+export const ZERO_INPUTS: number[] = [0, -0];
